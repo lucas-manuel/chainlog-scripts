f0cc0241a391a0e898960702b91837d856d1cbb3
Lucas <lucas@makerdao.com>
2020-10-14 17:14:50 -0400

    feat: add populate scripts for goerli, ropsten, rinkeby, kovan, mainnet

---
+++ README.md
@@ -0,0 +1,2 @@
+# chainlog-scripts
+Set of scripts to pull addresses from changelog site and populate chainlog
---
+++ populate-chainlog-kovan.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+[[ "$ETH_RPC_URL" && "$(seth chain)" == "kovan"  ]] || { echo "Please set a kovan ETH_RPC_URL"; exit 1;  }
+
+CHAINLOG=0xda0ab1e0017debcd72be8599041a2aa3ba7e740f
+GAS_PRICE=1000000000
+ETH_GAS=500000
+
+key_vals=$(curl https://changelog.makerdao.com/releases/kovan/1.1.2/contracts.json | jq -c -r 'keys[] as $k | "\($k)=\( .[$k])"')
+
+for row in $key_vals; do
+    key=$(echo $row | sed 's/=.*//')
+    address=$(echo $row | sed -e 's#.*=\(\)#\1#')
+    echo "Setting $key in chainlog to $address..."
+    seth send $CHAINLOG 'setAddress(bytes32,address)' $(seth --to-bytes32 $(seth --from-ascii $key)) $address
+    echo "$key set successfully"
+done
